3299ef97c3bcdd05e6c56475cddaff9345d58bab
Jimmy <rivendinner@gmail.com>
2024-07-31 18:29:22 +0800

    backup

--- Error/CombineErrors.go
+++ Error/CombineErrors.go
@@ -1,16 +1,16 @@
-package Common
+package Error
 
 type CombineErrors struct {
 	Errs []error
 }
 
-func NewCombineErrors() CombineErrors {
+func NewErrors() CombineErrors {
 	return CombineErrors{
 		Errs: []error{},
 	}
 }
 
-func (c *CombineErrors) AddError(err error) {
+func (c *CombineErrors) Add(err error) {
 	if err != nil {
 		c.Errs = append(c.Errs, err)
 	}
---
+++ Error/Error.go
@@ -0,0 +1,3 @@
+package Error
+
+func New() {}
