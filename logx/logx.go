a401b5fe7c7f79c93d3a1462af9adeabaa817d4f
qy <jimmy@ffglobaltech.com>
2025-07-11 11:23:47 +0800

    backup

--- logx/logx.go
+++ logx/logx.go
@@ -21,3 +21,23 @@ func Debugf(format string, args ...any) {
 func Fatalf(format string, args ...any) {
 	log.Fatal().Msgf(format, args...)
 }
+
+func Info(msg string) {
+	log.Info().Msg(msg)
+}
+
+func Error(msg string) {
+	log.Error().Msg(msg)
+}
+
+func Warn(msg string) {
+	log.Warn().Msg(msg)
+}
+
+func Debug(msg string) {
+	log.Debug().Msg(msg)
+}
+
+func Fatal(msg string) {
+	log.Fatal().Msg(msg)
+}
