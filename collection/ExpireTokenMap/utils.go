610e82c4f22e4e8335ee2e7a29a8324ebb02d0a2
qy <jimmy@ffglobaltech.com>
2025-09-03 18:29:28 +0800

    backup

--- collection/ExpireTokenMap/utils.go
+++
@@ -1,6 +0,0 @@
-package expiretokenmap
-
-type Options struct {
-	AdaptiveCleanInterval bool // Dynamic adjust clean interval to keep memory usage
-	TargetLen             int  // Target length with adaptive cleaning
-}
