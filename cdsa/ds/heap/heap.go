0d9a6c25b13d3f356a3ab458c6831eac0b0aa3f3
qy <jimmy@ffglobaltech.com>
2025-09-17 15:50:45 +0800

    backup

--- cdsa/ds/heap/heap.go
+++ cdsa/ds/heap/heap.go
@@ -1,8 +1,11 @@
 package heap
 
+import "sync"
+
 // Heap is a min-heap or max-heap implementation using generics.
 // It is 1-indexed, meaning the first element (index 0) is a dummy value.
 type Heap[T any] struct {
+	mu   sync.RWMutex // Add mutex for concurrency control
 	data []T
 	less func(T, T) bool // less(a, b) returns true if a < b (for min-heap) or a > b (for max-heap)
 }
@@ -18,7 +21,7 @@ func New[T any](eles []T, less func(T, T) bool) *Heap[T] {
 	}
 
 	h.data = append(h.data, eles...)
-	for i := h.Len() / 2; i > 0; i-- {
+	for i := (len(h.data) - 1) / 2; i > 0; i-- {
 		h.down(i)
 	}
 
@@ -26,24 +29,39 @@ func New[T any](eles []T, less func(T, T) bool) *Heap[T] {
 }
 
 // Len returns the number of elements in the heap.
-func (h *Heap[T]) Len() int { return len(h.data) - 1 }
+func (h *Heap[T]) Len() int {
+	h.mu.RLock()
+	defer h.mu.RUnlock()
+	return len(h.data) - 1
+}
 
-func (h *Heap[T]) swap(i, j int) { h.data[i], h.data[j] = h.data[j], h.data[i] }
+func (h *Heap[T]) len() int {
+	return len(h.data) - 1
+}
+
+func (h *Heap[T]) swap(i, j int) {
+	h.data[i], h.data[j] = h.data[j], h.data[i]
+}
 
 // Push adds an element to the heap.
 func (h *Heap[T]) Push(e T) {
+	h.mu.Lock()
+	defer h.mu.Unlock()
 	h.data = append(h.data, e)
-	h.up(h.Len())
+	h.up(h.len())
 }
 
 // Pop removes and returns the top element from the heap.
 func (h *Heap[T]) Pop() T {
-	if h.Len() == 0 {
+	h.mu.Lock()
+	defer h.mu.Unlock()
+
+	if h.len() == 0 {
 		var zeroValue T
 		return zeroValue // Or panic, depending on desired behavior for empty heap
 	}
 
-	n := h.Len()
+	n := h.len()
 	res := h.data[1]
 	h.data[1] = h.data[n]
 	h.data = h.data[:n] // Shrink the slice
@@ -58,8 +76,8 @@ func (h *Heap[T]) up(i int) {
 }
 
 func (h *Heap[T]) down(i int) {
-	for j := i << 1; j <= h.Len(); i, j = j, j<<1 {
-		if j+1 <= h.Len() && h.less(h.data[j+1], h.data[j]) {
+	for j := i << 1; j <= h.len(); i, j = j, j<<1 {
+		if j+1 <= h.len() && h.less(h.data[j+1], h.data[j]) {
 			j++
 		}
 		if h.less(h.data[j], h.data[i]) {
