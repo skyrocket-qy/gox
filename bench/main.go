664ea08875e9bbc92090f752c1af47b12ff8700f
qy <jimmy@ffglobaltech.com>
2025-09-17 13:52:25 +0800

    backup

--- bench/main.go
+++ bench/main.go
@@ -1,17 +1,57 @@
 package main
 
 import (
+	"fmt"
 	"log"
-	"time"
 
 	"github.com/skyrocket-qy/gox/bench/pkg"
 )
 
 func main() {
-	err := pkg.Bench(func() {
-		time.Sleep(10 * time.Millisecond)
-	}, 100, "bench.png")
+	err := pkg.Bench(bNumbers, 1, "bench.png")
 	if err != nil {
 		log.Fatalf("Bench failed: %v", err)
 	}
+
+	err = pkg.Bench(bt, 100, "bt.png")
+	if err != nil {
+		log.Fatalf("Bench failed: %v", err)
+	}
+}
+
+const t = 100000
+
+func numbers() <-chan int {
+	ch := make(chan int)
+	go func() {
+		defer close(ch) // close when done
+		for i := 0; i <= t; i++ {
+			ch <- i // "yield return i"
+		}
+	}()
+	return ch
+}
+
+func bNumbers() {
+	sum := 0
+	for v := range numbers() {
+		sum += v
+	}
+	fmt.Println(sum)
+}
+
+func tradit() []int {
+	res := []int{}
+	for i := 0; i < t; i++ {
+		res = append(res, i)
+	}
+	return res
+}
+
+func bt() {
+	sum := 0
+	for v := range tradit() {
+		sum += v
+	}
+	fmt.Println(sum)
 }
