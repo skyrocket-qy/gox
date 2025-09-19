bb48785b638cbd60ee2e91cd510ac363537fbdac
qy <jimmy@ffglobaltech.com>
2025-09-19 16:59:40 +0800

    backup

--- dsa/alg/unionfind/union_find.go
+++ dsa/alg/unionfind/union_find.go
@@ -5,12 +5,14 @@ package unionfind
 // UnionFind is a data structure that keeps track of a set of elements partitioned into a number of disjoint (non-overlapping) subsets.
 type UnionFind[T comparable] struct {
 	parents map[T]T
+	sizes   map[T]int // use to flatten
 }
 
 // NewUnionFind creates and returns a new UnionFind instance.
 func New[T comparable]() *UnionFind[T] {
 	return &UnionFind[T]{
 		parents: make(map[T]T),
+		sizes:   make(map[T]int),
 	}
 }
 
@@ -19,6 +21,7 @@ func (uf *UnionFind[T]) Find(x T) T {
 	// If x is not in the map, it's a new element, so set itself as the root.
 	if _, ok := uf.parents[x]; !ok {
 		uf.parents[x] = x
+		uf.sizes[x] = 1
 	}
 
 	// Path compression: If x is not the root, recursively find its root and set it as x's parent.
@@ -36,7 +39,11 @@ func (uf *UnionFind[T]) Union(x, y T) {
 
 	// If they are not already in the same set, merge them.
 	if rootX != rootY {
+		if uf.sizes[rootX] < uf.sizes[rootY] {
+			rootX, rootY = rootY, rootX
+		}
 		uf.parents[rootX] = rootY
+		uf.sizes[rootX] += uf.sizes[rootY]
 	}
 }
 
