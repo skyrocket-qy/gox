bee89a277d6c44d422a21db449c259e656290756
qy <jimmy@ffglobaltech.com>
2025-09-19 18:31:17 +0800

    backup

--- dsa/ds/heap/heap_test.go
+++ dsa/ds/heap/heap_test.go
@@ -92,9 +92,9 @@ func TestHeapWithInitialElements(t *testing.T) {
 
 type IntHeap []int
 
-func (h IntHeap) Len() int           { return len(h) }
-func (h IntHeap) Less(i, j int) bool { return h[i] < h[j] }
-func (h IntHeap) Swap(i, j int)      { h[i], h[j] = h[j], h[i] }
+func (h *IntHeap) Len() int           { return len(*h) }
+func (h *IntHeap) Less(i, j int) bool { return (*h)[i] < (*h)[j] }
+func (h *IntHeap) Swap(i, j int)      { (*h)[i], (*h)[j] = (*h)[j], (*h)[i] }
 
 func (h *IntHeap) Push(x any) {
 	*h = append(*h, x.(int))
