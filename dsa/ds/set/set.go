68afcfaa791364e697c14a9f272474bacb381df5
qy <jimmy@ffglobaltech.com>
2025-09-23 13:30:29 +0800

    backup

--- dsa/ds/set/set.go
+++ dsa/ds/set/set.go
@@ -1,1 +1,35 @@
 package set
+
+type Set[T comparable] map[T]struct{}
+
+func New[T comparable]() Set[T] {
+	return make(Set[T])
+}
+
+func (s Set[T]) Add(item T) {
+	s[item] = struct{}{}
+}
+
+func (s Set[T]) Remove(item T) {
+	delete(s, item)
+}
+
+func (s Set[T]) Contains(item T) bool {
+	_, ok := s[item]
+
+	return ok
+}
+
+func (s Set[T]) Size() int {
+	return len(s)
+}
+
+func (s Set[T]) IsEmpty() bool {
+	return len(s) == 0
+}
+
+func (s Set[T]) Clear() {
+	for k := range s {
+		delete(s, k)
+	}
+}
